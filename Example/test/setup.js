4b66e46611c5d23af9dc1b5a5ba8086752fef6df
long.le <long.le@tiki.vn>
2016-09-15 14:14:10 +0700

    [Redux-react] add Example

--- Example/test/setup.js
+++ Example/test/setup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {expect} from 'chai';
 import jsdom from 'jsdom';
+
 const document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 const window = document.defaultView;
 
@@ -8,7 +9,7 @@ global.document = document;
 global.window = window;
 
 Object.keys(window).forEach((key) => {
-    if(!(key in global)){
+    if (!(key in global)) {
         global[key] = window[key];
     }
 });
