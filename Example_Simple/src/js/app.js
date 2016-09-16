ede90e931d34620b16a09fec6c7c1e789fe254f9
long.le <long.le@tiki.vn>
2016-09-16 16:45:34 +0700

    [Redux-react] new project simple

--- Example_Simple/src/js/app.js
+++ Example_Simple/src/js/app.js
@@ -36,3 +36,141 @@ function clearTodo() {
     };
 }
 
+function todoApp(state, action) {
+    switch (action.type){
+        case 'ADD_TODO':
+            var items = [].concat(state.todo.items);
+            return Object.assign({}, state, {
+                todo: {
+                    items: items.concat([{
+                        message: action.message,
+                        completed: false
+                    }])
+                }
+            });
+        case 'COMPLETE_TODO':
+            var items = [].concat(state.todo.items);
+            items[action.index].completed = true;
+            return Object.assign({}, state, {
+                todo: {
+                    items: items
+                }
+            });
+        case 'DELETE_TODO':
+            var items = [].concat(state.todo.items);
+            items.splice(action.index, 1);
+            return Object.assign({}, state, {
+                todo: {
+                    items: items
+                }
+            });
+        case 'CLEAR_TODO':
+            return Object.assign({}, state, {
+                todo: {
+                    items: []
+                }
+            });
+    }
+}
+
+var store = createStore(todoApp, defaultState);
+
+class AddTodoForm extends React.Component{
+    state = {
+        message: ''
+    };
+
+    onFormSumit(e){
+        e.preventDefault();
+        store.dispatch(addTodo(this.state.message));
+        this.setState({ message: '' });
+    }
+
+    onMessageChanged(e){
+        var message = e.target.value;
+        this.setState({ message: message });
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.onFormSumit.bind(this)}>
+                <input type="text" placeholder="Todo..." onChange={this.onMessageChanged.bind(this)} value={this.state.message} />
+                <input type="submit" value="Add" />
+            </form>
+        );
+    }
+}
+
+class TodoItem extends React.Component{
+    onDeleteClick(){
+        store.dispatch(deleteTodo(this.props.index));
+    }
+
+    onCompletedClick(){
+        store.dispatch(completeTodo(this.props.index));
+    }
+
+    render(){
+        return(
+            <li>
+                <a href="javascript:;" onClick={this.onCompletedClick.bind(this)}
+                    style={{textDecoration: this.props.completed? 'line-through': 'none'}}
+                >
+                    {this.props.message.trim()}
+                </a>
+                <a href="javascript:;" onClick={this.onDeleteClick.bind(this)} style={{textDecoration: 'none'}} >[x]</a>
+            </li>
+        );
+    }
+}
+
+class TodoList extends React.Component{
+    state = {
+        items: []
+    };
+
+    componentWillMount(){
+        store.subscribe(() => {
+            var state = store.getState();
+            this.setState({
+                items: state.todo.items
+            });
+        });
+    }
+
+    render(){
+        var items = [];
+
+        this.state.items.forEach((items, index) => {
+            items.push(
+                <TodoItem
+                    key={index}
+                    index={index}
+                    message={items.message}
+                    completed={items.completed}
+                />
+            );
+        });
+
+        if(!items.length){
+            return(
+                <p>
+                    <i>Please add something to do.</i>
+                </p>
+            );
+        }
+
+        return(
+            <ol>{items}</ol>
+        )
+    }
+}
+
+ReactDOM.render(
+    <div className="wrapper">
+        <h1>Todo</h1>
+        <AddTodoForm />
+        <TodoList />
+    </div>,
+    document.getElementById('container')
+);
