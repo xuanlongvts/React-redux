233f41a04e5088708318841bef26fc53f8161375
long.le <long.le@tiki.vn>
2016-11-04 17:37:27 +0700

    [Redux-react] add structure for a bigger project

---
+++ Section_1_BasicRedux&PlainJavascript/structureForABiggerProject/reducers/counter.js
@@ -0,0 +1,20 @@
+function counter(currentState, action) {
+    DEFAULT_STATE = 0;
+
+    if(currentState === undefined){
+        nextState = DEFAULT_STATE;
+        return nextState;
+    }
+
+    switch (action.type){
+        case 'DECREMENT':
+            nextState = currentState - 1;
+            return nextState;
+        case 'INCREMENT':
+            nextState = currentState + 1;
+            return nextState;
+        default:
+            nextState = currentState;
+            return nextState;
+    }
+}
--- Section_1_BasicRedux&PlainJavascript/structureForABiggerProject/reducers/index.js
+++ Section_1_BasicRedux&PlainJavascript/structureForABiggerProject/reducers/index.js
@@ -1,47 +1,8 @@
-function counter(currentState, action) {
-    DEFAULT_STATE = 0;
-
-    if(currentState === undefined){
-        nextState = DEFAULT_STATE;
-        return nextState;
-    }
-
-    switch (action.type){
-        case 'DECREMENT':
-            nextState = currentState - 1;
-            return nextState;
-        case 'INCREMENT':
-            nextState = currentState + 1;
-            return nextState;
-        default:
-            nextState = currentState;
-            return nextState;
-    }
-}
-
-function sum(currentState, action) {
-    var DEFAULT_STATE = 3;
-
-    if(currentState === undefined){
-        nextState = DEFAULT_STATE;
-        return nextState;
-    }
-
-    switch (action.type){
-        case 'SUM':
-            nextState = parseInt(action.a) + parseInt(action.b);
-            return nextState;
-        default:
-            nextState = currentState;
-            return nextState;
-    }
-}
-
 function combineReducer(currentState, action) {
-    var DEFAULT_STATE = {
+    /*var DEFAULT_STATE = {
         count: 0,
         sum: 3
-    };
+    };*/
     var nextState = Object.assign({}, currentState);
     nextState = {
         count: counter(nextState.count, action),
