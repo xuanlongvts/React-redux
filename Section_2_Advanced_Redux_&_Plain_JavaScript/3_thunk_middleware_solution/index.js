55425c6bf6ffd40193f76b5bbd7d34c497137190
long.le <long.le@tiki.vn>
2016-11-09 11:56:51 +0700

    [Redux-react] middleware thunk solution

---
+++ Section_2_Advanced_Redux_&_Plain_JavaScript/3_thunk_middleware_solution/index.js
@@ -0,0 +1,27 @@
+var store = Redux.createStore(combineReducer, Redux.applyMiddleware(logger, crashReporter, thunk));
+
+function render() {
+    var state = store.getState();
+    document.getElementById('value').innerHTML = state.count.result;
+    document.getElementById('value2').innerHTML = state.sum;
+
+    if(state.count.loading){
+        document.getElementById('status').innerHTML = 'is loading...';
+    }
+    else{
+        document.getElementById('status').innerHTML = 'loaded';
+    }
+
+    document.getElementById('imagesStatus').innerHTML = state.images.loading;
+    if(state.images.loading == 'loading...'){
+        document.getElementById('imagesList').innerHTML = '';
+    }
+    else if(state.images.loading == 'loaded'){
+        for(var i = 0; state.images.data.length; i++){
+            document.getElementById('imagesList').innerHTML += ("<img src=" + state.images.data[i].link +" style='height: 200px;' />");
+        }
+    }
+}
+
+store.subscribe(render);
+render();
