b197ee89afb0c11805b76e8baed075c3f52e49bd
long.le <long.le@tiki.vn>
2016-11-18 17:34:26 +0700

    [Redux-react] React props

---
+++ Section_5_React/5_props/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Counter2, { a as a2, b} from './components/Counter';
+import Sum from './components/Sum';
+
+class Describe extends Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        console.log('Describe.render()');
+        return(
+            <div>
+                <div style={{color: 'red'}}>
+                    Describe: {this.props.desc}
+                </div>
+            </div>
+        )
+    }
+}
+
+class Exmples extends Component{
+    constructor(props){
+        super(props);
+        this.state = {describe: 'none'};
+        this.describe = this.updateDescribe.bind(this);
+    }
+
+    updateDescribe(describe){
+        this.setState({describe: describe});
+    }
+
+    render(){
+        console.log('Example.render()');
+        return(
+            <div>
+                <h1>React</h1>
+                <Describe desc = {this.state.describe} />
+                <Counter2 />
+                <Sum sumDesc = {this.updateDescribe} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Exmples/>,
+    document.getElementById('root')
+);
