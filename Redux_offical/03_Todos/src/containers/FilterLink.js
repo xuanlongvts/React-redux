39197367445ca23a0369d36aeaabfc5bf4b7362d
long.le <long.le@tiki.vn>
2016-12-26 17:28:09 +0700

    [Redux-react] Todos

--- Redux_offical/03_Todos/src/containers/FilterLink.js
+++ Redux_offical/03_Todos/src/containers/FilterLink.js
@@ -3,6 +3,10 @@ import {setVisibilityFilter} from '../actions/index';
 import Link from '../components/Link';
 
 const mapStateToProps = (state, ownProps) => {
+
+    console.log('state: ', state);
+    console.log('ownProps: ', ownProps);
+
     return{
         active: ownProps.filter === state.visibilityFilter
     }
