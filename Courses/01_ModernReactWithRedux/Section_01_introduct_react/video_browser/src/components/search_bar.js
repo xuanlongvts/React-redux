106a89ae941de306db4d8559fce8a45ae2fd981e
xuanlongvts <xuanlongvts@gmail.com>
2017-04-02 23:40:37 +0700

    'video'

---
+++ Courses/01_ModernReactWithRedux/Section_01_introduct_react/video_browser/src/components/search_bar.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: ''
+		}
+	}
+
+	onInputChange(term){
+		this.setState({
+			term: term
+		});
+		console.log('term: ', term);
+		//this.props.onSearchTermChange(term);
+	}
+
+	render(){
+		return(
+			<div className="search-bar">
+				<input 
+					value={this.state.term}
+					onChange={e => this.onInputChange(e.target.value)}
+				/>
+			</div>
+		)
+	}
+}
+
+export default SearchBar;
