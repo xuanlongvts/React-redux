2a4cfbc8c34e03e5497e5ba0c98f39f31329991b
xuanlongvts <xuanlongvts@gmail.com>
2017-04-16 14:24:34 +0700

    'react-router'

--- Section_8_React_Redux_Router/2_React_Router_IndexRoute/components/App.js
+++ Section_8_React_Redux_Router/2_React_Router_IndexRoute/components/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import {Link, browserHistory} from 'react-router';
 
-export default function App({children}) {
+// class App extends Component {
+//     render(){
+//         const {children} = this.props;
+//         return(
+//             <div>
+//                 <header>
+//                     Links:
+//                     &nbsp;&nbsp;&nbsp;&nbsp;
+//                     <Link to="/">Home</Link>
+//                     &nbsp;&nbsp;&nbsp;&nbsp;
+//                     <Link to="/ex02">Example02</Link>
+//                     &nbsp;&nbsp;&nbsp;&nbsp;
+//                     <Link to="/ex02/NongNo">Example02 with parameter</Link>
+//                 </header>
+//                 <div>
+//                     <br />
+//                     <button onClick={()=> browserHistory.push('/ex02')}>Go to /Example02</button>
+//                 </div>
+//                 <div style={{marginTop: '1.5em'}}>{children}</div>
+//             </div>
+//         )
+//     }
+    
+// }
+
+// export default App;
+
+const App = ({children}) => {
     return(
         <div>
             <header>
@@ -21,3 +49,5 @@ export default function App({children}) {
         </div>
     )
 }
+
+export default App;
